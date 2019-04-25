9ee06b2a614c032c96904017a0d01c72f9a21512
awakening <lucidsunlight@yandex.ru>
2019-04-25 19:29:31 +0700

    Disable `@typescript-eslint/explicit-function-return-type`

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,19 @@ module.exports = {
   extends: ["airbnb", "plugin:@typescript-eslint/recommended", "react-app"],
   plugins: ["@typescript-eslint", "react"],
   rules: {
-    "@typescript-eslint/indent": ["error", 2]
+    "@typescript-eslint/indent": ["error", 2],
+    "@typescript-eslint/explicit-function-return-type": "off"
   },
   overrides: [
     {
-      // disable TS rules for `.js` files
+      // disable TS rules for JS files
       files: ["*.js"],
       rules: {
         "@typescript-eslint/no-var-requires": "off"
       }
     },
     {
+      // disable JS rules for TS files
       files: ["*.tsx"],
       rules: {
         "react/jsx-filename-extension": "off"
