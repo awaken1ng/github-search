2a9b1effec54fd71141a3e843fab9e13deb3bd5b
awakening <lucidsunlight@yandex.ru>
2019-04-25 17:10:47 +0700

    Add `#` path resolver to `src`

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,10 @@
-module.exports = function override(config, env) {
-  return config;
+const path = require('path');
+
+module.exports = {
+  webpack: function(config, env) {
+    console.log(config);
+    // ...add your webpack config
+    config.resolve.alias['#'] = path.resolve('./src');
+    return config;
+  }
 }
