440aa8e6e48dc249e554767d985d3c2b709a47af
awakening <lucidsunlight@yandex.ru>
2019-05-04 18:01:32 +0700

    Add next rate limit reset to status bar

--- src/components/StatusBar.tsx
+++ src/components/StatusBar.tsx
@@ -1,28 +1,27 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
+import moment from 'moment';
 import { StoreState } from '../state-store';
 import './StatusBar.sass';
 
 interface Props {
   remaining: number;
   limit: number;
+  reset: number;
 }
 
 export function StatusBar({
-  remaining, limit,
+  remaining, limit, reset,
 }: Props) {
   return (
     <div className="statusBar">
-      Rate Limit: {remaining}/{limit}
+      Rate Limit: {remaining}/{limit} Next reset: {moment.utc(reset * 1000).fromNow()}
     </div>
   );
 }
 
 function mapStateToProps(state: StoreState) {
-  return {
-    remaining: state.ratelimit.remaining,
-    limit: state.ratelimit.limit,
-  };
+  return state.ratelimit;
 }
 
 export default connect(mapStateToProps)(StatusBar);
